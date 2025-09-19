f52ad0e155e6fbb2a99bb6b13abd3a24e2db09f8
GautamGhand <gautamghand820@gmail.com>
2025-09-19 20:23:53 +0530

    ng for directive completed

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -4,12 +4,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [NgIf, NgFor],
   templateUrl: './directives.component.html',
-  styleUrl: './directives.component.css'
+  styleUrl: './directives.component.css',
 })
 export class DirectivesComponent {
-  bgColor:string='red';
-  show:boolean=false;
-  students=["Anil","Sam","Peter","Vinay","Bruce"];
+  bgColor: string = 'red';
+  show: boolean = false;
+  students = ['Anil', 'Sam', 'Peter', 'Vinay', 'Bruce'];
+  studentsData:{id:number,name:string,age:number,email:string}[] = [
+    {
+      id:1,
+      name: 'Anil',
+      age: 18,
+      email: 'anil@sidhu.com',
+    },
+    {
+      id:2,
+      name: 'Test',
+      age: 26,
+      email: 'test@sidhu.com',
+    },
+    {
+      id:3,
+      name: 'Gautam',
+      age: 30,
+      email: 'gautam@sidhu.com',
+    },
+  ];
 }
